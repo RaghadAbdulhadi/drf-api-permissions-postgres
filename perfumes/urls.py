8f9acb959a127478527b8c60c93aa8a27c9b1dbb
RaghadAbdulhadi <raghadabdulhadi1999@gmail.com>
2022-06-04 23:08:54 +0300

    Modification

--- perfumes/urls.py
+++ perfumes/urls.py
@@ -19,5 +19,5 @@ from django.urls import path, include
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('api/v1/', include('perfumes_blog.urls')),
-
+    path('api-auth/', include('rest_framework.urls')),
 ]
